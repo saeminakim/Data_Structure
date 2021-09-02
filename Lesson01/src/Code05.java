eae55a10102dd10fba1dfbf268275f05f365c1b6
Lily <helloimlily@gmail.com>
2021-09-02 21:02:06 +0900

    0902 Array

---
+++ Lesson01/src/Code05.java
@@ -0,0 +1,22 @@
+
+public class Code05 {
+
+	public static void main(String[] args) {
+
+		int n = 5;
+		int [] grades = new int [n];  // variable length array
+		
+		grades[0] = 100;
+		grades[1] = 76;
+		grades[2] = 92;
+		grades[3] = 95;
+		grades[4] = 14;
+		
+		System.out.println(grades[0]);
+		System.out.println(grades[1]);
+		System.out.println(grades[2]);
+		System.out.println(grades[3]);
+		System.out.println(grades[4]);
+	}
+
+}
