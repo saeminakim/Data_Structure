927b73c01ff2c235ae5d196760df21eca9576ecf
Lily <helloimlily@gmail.com>
2021-09-30 16:19:56 +0900

    0930 Class 5

---
+++ Chapter2/src/Section1/Polynomial.java
@@ -0,0 +1,7 @@
+package Section1;
+
+public class Polynomial { // 하나의 다항식을 표현하기 위한 클래스
+	public char name;
+	public Term[] terms;
+	public int nTerms = 0; // 항의 갯수
+}
---
+++ Chapter2/src/Section1/Term.java
@@ -0,0 +1,6 @@
+package Section1;
+
+public class Term {  // 하나의 항을 표현하기 위한 클래스
+	public int coef;
+	public int expo;
+}
