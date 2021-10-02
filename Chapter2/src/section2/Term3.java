a2573231d6c4e749224f3dab81d2e14df8ffd4f6
Lily <helloimlily@gmail.com>
2021-10-02 20:05:03 +0900

    1002 Method and Constructor 2

---
+++ Chapter2/src/section2/Term3.java
@@ -0,0 +1,19 @@
+package section2;
+
+public class Term3 {  // 하나의 항을 표현하기 위한 클래스
+	public int coef;
+	public int expo;
+	
+	public Term3(int c, int e) {
+		coef = c;
+		expo = e;
+	}
+	
+	public int calcTerm(int x) {
+		return (int) (coef * Math.pow(x, expo));
+	}
+	
+	public void printTerm() {
+		System.out.print(coef + "x^" + expo);
+	}
+}
