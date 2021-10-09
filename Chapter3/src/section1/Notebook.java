0ca70af242c30a793f4e9ae7d64ebe5aaf15afd6
Lily <helloimlily@gmail.com>
2021-10-09 18:08:55 +0900

    1009 Polymorphism

--- Chapter3/src/section1/Notebook.java
+++ Chapter3/src/section1/Notebook.java
@@ -21,10 +21,17 @@ public class Notebook extends Computer {  // extends 하나의 클래스를 확장한다는 
 	}
 	
 	public static void main(String[] args) {
-		Notebook test = new Notebook("Dell", "i5", 4, 1000, 3.2, 15.6, 1.2);
-		
+		// Notebook test = new Notebook("Dell", "i5", 4, 1000, 3.2, 15.6, 1.2);
+		Computer test = new Notebook("Dell", "i5", 4, 1000, 3.2, 15.6, 1.2);
+		// 다형성
+		// Computer 타입의 변수 test가 Notebook 타입의 객체를 참조하고 있다.		
+		// Notebook test2 = new Computer("Dell", "i5", 4, 1000, 3.2);  // 에러 발생
+		// 반대의 경우는 안됨
+
 		System.out.println(test.computePower());
-		System.out.println(test.toString());
+		System.out.println(test.toString());  
+		// static binding vs. dynamic binding
+		// Notebook class의 toString 메서드 호출됨. dynamic binding
 		
 	}
 	
