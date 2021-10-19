539325145464be7cdcb62df89f479e22389c2496
Lily <helloimlily@gmail.com>
2021-10-19 23:23:38 +0900

    1019 Scheduler 2

--- Chapter3/src/section2/Scheduler.java
+++ Chapter3/src/section2/Scheduler.java
@@ -6,14 +6,22 @@ public class Scheduler {
 	
 	public Event[] events = new Event[100];
 	public int n = 0;
+	private Scanner kb;
 
 	public void processCommand() {
-		Scanner kb = new Scanner(System.in);
+		kb = new Scanner(System.in);
 		while(true) {
 			System.out.println("$ ");
 			String command = kb.next();
 			if(command.equals("addevent")) {
-
+				String type = kb.next();
+				if(type.equalsIgnoreCase("oneday")) {
+					handleAddOneDayEvent();
+				} else if (type.equalsIgnoreCase("duration")) {
+					handleAddDurationEvent();
+				} else if (type.equalsIgnoreCase("deadline")) {
+					handleAddDeadlinedEvent();
+				} 
 			}
 			else if(command.equals("list")) {
 
@@ -26,6 +34,40 @@ public class Scheduler {
 			}
 		}
 		kb.close();
+	}
+
+	private void handleAddDeadlinedEvent() {
+		
+	}
+
+	private void handleAddDurationEvent() {
+		
+	}
+
+	private void handleAddOneDayEvent() {
+		System.out.print(" when : ");
+		String dateString = kb.next();
+		System.out.print(" title : ");
+		String title = kb.next();
+		
+		MyDate date = parseDateString(dateString);
+				
+		OneDayEvent ev = new OneDayEvent(title, date);
+		
+		System.out.println(ev.toString());
+		events[n++] = ev;
+	}
+
+	private MyDate parseDateString(String dateString) {
+		String[] tokens = dateString.split("/");
+		
+		int year = Integer.parseInt(tokens[0]);
+		int month = Integer.parseInt(tokens[1]);
+		int day = Integer.parseInt(tokens[2]);
+		
+		MyDate d = new MyDate(year, month, day);
+		
+		return d;
 	}
 
 	public static void main(String[] args) {
