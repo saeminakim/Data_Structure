6647c411054c35e62d0a2eac49191b9f593a2f51
Lily <helloimlily@gmail.com>
2021-10-22 22:19:49 +0900

    1022 Abstract

---
+++ Chapter3/src/section4/OneDayEvent.java
@@ -0,0 +1,18 @@
+package section4;
+
+public class OneDayEvent extends Event {
+	public MyDate date;
+	
+	public OneDayEvent(String title, MyDate date) {
+		super(title);
+		this.date = date;
+	}
+	
+	public boolean isRelevant(MyDate date) {		
+		return this.date.compareTo(date) == 0;
+	}
+	
+	public String toString() {
+		return title + ", " + date.toString();
+	}
+}
