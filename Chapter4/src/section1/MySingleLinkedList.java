a5de60768ee006f8c0572674dbae128b603f61cc
Lily <helloimlily@gmail.com>
2021-11-04 22:18:44 +0900

    1104 LinkedList

--- Chapter4/src/section1/MySingleLinkedList.java
+++ Chapter4/src/section1/MySingleLinkedList.java
@@ -1,68 +1,106 @@
 package section1;
 
 public class MySingleLinkedList<T> {
-	
+
 	public Node<T> head; // 첫번째 노드의 주소
 	public int size;
-	
+
 	public MySingleLinkedList() {
 		head = null;
 		size = 0;
 	}
-	
+
 	public void addFirst(T item) {
 		Node<T> newNode = new Node<T>(item);		
 		newNode.next = head;
 		head = newNode;
 		size++;
 	}
-	
+
 	public void addAfter(Node<T> before, T item) {
 		Node<T> temp = new Node<T>(item);
 		temp.next = before.next;
 		before.next = temp;
 		size++;
 	}
-	
+
 	public T removeFirst() {
 		if(head == null)
 			return null;
-		
+
 		T tmp = head.data;
 		head = head.next;
 		size--;
-		
+
 		return tmp;
 	}
-	
+
 	public T removeAfter(Node<T> before) {
 		if(before.next == null)
 			return null;
-		
+
 		T tmp = before.next.data;
 		before.next = before.next.next;
 		size--;
-		
+
 		return tmp;
 	}
-	
-	public void add(int index, T item) {  // insert
-		
+
+	public int indexOf(T item) {
+		Node<T> p = head;
+		int index = 0;
+		while(p != null) {
+			if(p.data.equals(item))
+				return index;
+			p = p.next;
+			index++;
+		}
+
+		return -1;
 	}
-	
-	public void remove(int index) {  // delete
-		
+
+	public Node<T> getNode(int index){
+		if(index < 0 || index >= size)
+			return null;
+		Node<T> p = head;
+		for(int i = 0; i < index; i++) 
+			p = p.next;
+		return p;			
 	}
-	
+
 	public T get(int index) {
-		return null;
+		if(index < 0 || index >= size)
+			return null;
+
+		//		Node<T> p = head;
+		//		for(int i = 0; i < index; i++) 
+		//			p = p.next;
+		//		return p.data;	
+
+		return getNode(index).data;
 	}
-	
-	public int indexOf(T item) {  // search
+
+	public void add(int index, T item) {  // insert
+		if(index < 0 || index > size)
+			return;
 		
-		return 0;
+		if(index == 0)
+			addFirst(item);
+		else {
+			Node<T> q = getNode(index-1);
+			addAfter(q, item);
+		}
 	}
-	
+
+	public T remove(int index) {  // delete
+		if(index < 0 || index >= size)
+			return null;
+		if(index == 0)
+			return removeFirst();
+		Node<T> prev = getNode(index-1);
+		return removeAfter(prev);
+	}
+
 	public static void main(String[] args) {
 
 	}
